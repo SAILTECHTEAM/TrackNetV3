d09867d890d26b3edc270be7962fa7cde7b5fbfb
Louis LUO <ssparkluo@gmail.com>
2026-01-26 07:41:58 +0000

    better folder structure

--- tracknetv3/evaluation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Evaluation utilities for TrackNetV3."""
-
-from tracknetv3.evaluation.ensemble import get_ensemble_weight
-from tracknetv3.evaluation.metrics import evaluate
-from tracknetv3.evaluation.predict import predict_location
-
-__all__ = ["predict_location", "get_ensemble_weight", "evaluate"]
--- tracknetv3/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Utility functions for TrackNetV3."""
-
-# Export the utils package submodule. Avoid wildcard imports in package
-# __init__ to keep linting tools happy (F403).
-from . import general, trajectory
-
-__all__ = ["general", "trajectory", "generate_inpaint_mask", "linear_interp"]
-
-from .trajectory import generate_inpaint_mask, linear_interp
